9d7f495e1cd9094f4e8c066191daf84a73ff0ebf
greyfoxone <woj.mueller@gmx.de>
2025-06-19 18:10:06 +0200

    extra generations params for cfg adjust script

--- scripts/cfg_adjust.py
+++ scripts/cfg_adjust.py
@@ -4,20 +4,19 @@ import gradio as gr
 import matplotlib.pyplot as plt
 import modules.scripts as scripts
 import numpy as np
-from modules import processing
 from modules import shared
 from modules.infotext_utils import PasteField
 from PIL import Image
 
 
 class Script(scripts.Script):
-    cfg_fns = {
-        "Linear": lambda start, end, x: start + (end - start) * x,
-        "Square": lambda start, end, x: start + (end - start) * x**2,
-        "Square Root": lambda start, end, x: start + (end - start) * x**0.5,
-        # other fns
-        "x^4": lambda start, end, x: start + (end - start) * x**4,
+    cfg_fns_dict = {
         "x^0.25": lambda start, end, x: start + (end - start) * x**0.25,
+        "x^4": lambda start, end, x: start + (end - start) * x**4,
+        "Square Root": lambda start, end, x: start + (end - start) * x**0.5,
+        "Square": lambda start, end, x: start + (end - start) * x**2,
+        "Linear": lambda start, end, x: start + (end - start) * x,
+        # other fns
     }
 
     def title(self):
@@ -27,7 +26,7 @@ class Script(scripts.Script):
         return scripts.AlwaysVisible
 
     def ui(self, is_img2img):
-        with gr.Accordion(open=False, label=self.title()):
+        with gr.Accordion(open=True, label=self.title()):
             enabled = gr.Checkbox(label="Enabled", value=False)
             cfg_start = gr.Slider(
                 minimum=1, maximum=14, step=0.5, label="CFG Start", value=7.5
@@ -35,30 +34,41 @@ class Script(scripts.Script):
             cfg_end = gr.Slider(
                 minimum=1, maximum=14, step=0.5, label="CFG End", value=3.0
             )
+            choices = list(self.cfg_fns_dict.keys())
             curve = gr.Dropdown(
-                label="CFG Curve",
-                choices=self.cfg_fns.keys(),
+                type="value",
                 value="x^0.25",
+                label="CFG Curve",
+                choices=choices,
+                #                allow_custom_value=True,
             )
-            curve_graph = gr.Image(type="pil", visible=False)
+
+            curve_graph = gr.Image(
+                type="pil", visible=False, interactive=False, show_label=False
+            )
             curve.select(
                 fn=self.update_graph,
                 inputs=[curve, cfg_start, cfg_end],
                 outputs=[curve_graph],
             )
-        self.infotext_fields = [
-            PasteField(enabled, f"{self.title()}_enabled"),
-            PasteField(cfg_start, f"{self.title()}_cfg_start"),
-            PasteField(cfg_end, f"{self.title()}_cfg_end"),
-            PasteField(curve, f"{self.title()}_curve"),
-        ]
-        return [enabled, cfg_start, cfg_end, curve]
 
+        output = [enabled, cfg_start, cfg_end, curve]
+
+        self.infotext_fields = [
+            PasteField(component,self. info_field_for(component))
+            for component in output
+        ]
+
+        return output
+    
+    def info_field_for(self,component):
+            return f"{self.title()}/{component.label}"
+            
     def update_graph(self, curve, cfg_start, cfg_end):
         steps = np.linspace(0, 1, 100)
-        cfg_values = self.cfg_fns[curve](cfg_start, cfg_end, steps)
+        cfg_values = self.cfg_fns_dict[curve](cfg_start, cfg_end, steps)
 
-        plt.figure(figsize=(6, 4))
+        plt.figure(figsize=(4, 3))
         plt.plot(steps, cfg_values, label=curve)
         plt.xlabel("Progress")
         plt.ylabel("CFG Value")
@@ -84,7 +94,7 @@ class Script(scripts.Script):
             x, sigma = args
             step = shared.state.sampling_step
             progress = step / (p.steps - 1) if p.steps > 1 else 0
-            kwargs["cond_scale"] = self.cfg_fns[curve](
+            kwargs["cond_scale"] = self.cfg_fns_dict[curve](
                 cfg_start, cfg_end, progress
             )
             return args, kwargs
@@ -93,30 +103,17 @@ class Script(scripts.Script):
         self._handle = model.register_forward_pre_hook(
             cfg_hook, with_kwargs=True
         )
+        p.extra_generation_params.update(
+            #dict from self.infotext_fields
+            {self.infotext_fields[i][1] : value for i,value in enumerate(args)}
+        )
 
     def postprocess(self, p, processed, *args, **kwargs):
         (enabled, cfg_start, cfg_end, curve) = args
 
         if not enabled:
             return
-
-        # Generate CFG vs Steps graph
-        steps = np.arange(p.steps)  # 0 to steps-1
-        cfg_values = [
-            self.cfg_fns[curve](
-                cfg_start, cfg_end, step / (p.steps - 1) if p.steps > 1 else 0
-            )
-            for step in steps
-        ]
-
+        print(processed.infotexts)
         # Cleanup
         if hasattr(p, "_cfg_hook_handle"):
             self._handle.remove()
-
-    def run(self, p, *args, **kwargs):
-        (enabled, cfg_start, cfg_end, curve) = args
-
-        if not enabled:
-            return
-
-        return processing.process_images(p)
