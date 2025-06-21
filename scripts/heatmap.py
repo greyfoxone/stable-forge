d8fc0dd630fd942836cf1e3ffe117110f0d8d027
greyfoxone <woj.mueller@gmx.de>
2025-06-22 00:11:20 +0200

    refac heatmap to render grid with PIL

--- scripts/heatmap.py
+++ scripts/heatmap.py
@@ -1,15 +1,33 @@
 # Heat Map Script
-import io
-from typing import Tuple
 
 import gradio as gr
+import numpy as np
 import torch
+from matplotlib import cm
 from modules import scripts
 from modules.processing import StableDiffusionProcessing
 from PIL import Image
-from matplotlib import cm
-import numpy as np
 
+class SdBlockState:
+    def __init__(self, h: torch.Tensor, hsp: torch.Tensor, block_info):
+        self.h = h
+        self.hsp = hsp
+        self.block_nr = block_info[1]
+
+
+def get_block_state(unet, block_states):
+    def output_block_patch(
+        h: torch.Tensor, hsp: torch.Tensor, transformer_options
+    ):
+        state = SdBlockState(h.cpu(), hsp.cpu(), transformer_options["block"])
+        if state.block_nr not in block_states:
+            block_states[state.block_nr] = []
+        block_states[state.block_nr].append(state)
+        return h, hsp
+
+    unet_patched = unet.clone()
+    unet_patched.set_model_output_block_patch(output_block_patch)
+    return unet_patched
 
 class Heatmap(scripts.Script):
     def title(self):
@@ -43,6 +61,17 @@ class Heatmap(scripts.Script):
             self.on_after_component(
                 get_output_gallery, elem_id="txt2img_gallery"
             )
+            block_nr_slider.change(
+                    fn=self.update_heatmaps,
+                    inputs=[enabled, block_nr_slider, step_slider],
+                    outputs=[heatmap_gallery],
+                )
+            step_slider.change(
+                fn=self.update_heatmaps,
+                inputs=[enabled, block_nr_slider, step_slider],
+                outputs=[heatmap_gallery],
+            )
+                        
         return [enabled, block_nr_slider, step_slider, heatmap_gallery]
 
     def setup(self, p: StableDiffusionProcessing, *args):
@@ -50,139 +79,55 @@ class Heatmap(scripts.Script):
         if enabled is not True:
             return
         self.block_states = {}
+        self.renderer = FeatureRenderer()
 
     def update_heatmaps(self, enabled, block_nr_slider, step_slider):
         if enabled is not True:
             return
-
-        if not self.block_states:
+        
+        if hasattr(self,'block_states') == False:
             return
-
+        
+        print(self.block_states)
+        
         block_state = self.block_states[int(block_nr_slider)][int(step_slider)]
-
-        caption = f"Block: {block_nr_slider}  Step {step_slider}"
-        return self.cond_uncond_diffs(block_state, caption)
-
-    # takes the diff of cond and uncond for each feature
-    # and renders that diff
-    def cond_uncond_diffs(self, block_state, caption):
-        grid_size = 4
-        grid_col = 2
-        heatmaps = []
-        features = block_state.backbone[1] - block_state.backbone[0]
-        for i in range(0,len(features),grid_size): 
-            grid = HeatmapGrid(size=grid_size, caption=caption + f"\nfeatures {i} - {i+grid_size}", cols=grid_col)
-
-            for j, feature in enumerate(features[i:i+grid_size]):
-                grid.axes.flat[j].set_title(f"Feature {i+j+1}")
-                grid.axes.flat[j].imshow(feature.detach().numpy(), cmap="viridis")
-                
-    
-            heatmaps.append(grid.render())
-        return heatmaps
-
-    # calculates the mean of each feature and renders the diff
-    # of each feature from the mean
-    def deviation_from_mean(self, block_state, caption):
-        features = block_state.backbone.mean(dim=0)
-        mean = torch.mean(features, dim=0)
-        grid = HeatmapGrid(size=len(features), caption=caption, cols=32)
-
-        print(f"Drawing Features for {caption}")
-        for i, feature in enumerate(features):
-            grid.axes.flat[i].imshow(feature - mean, cmap="viridis")
-        print("Done")
-
-        heatmaps = grid.render()
-        return [heatmaps]
-
-    def process_before_every_sampling(
-        self, p: StableDiffusionProcessing, *args, **kwargs
-    ):
-        enabled, block_nr_slider, step_slider, heatmap_gallery = args
-        if enabled is not True:
-            return
-
-        unet = p.sd_model.forge_objects.unet
-        unet_patched = store_block_state(unet, self.block_states)
-        p.sd_model.forge_objects.unet = unet_patched
-        return
-
-
-# Tensor Dimension is (B=2,C,W,H)
-# B=2 because it's the cond and uncond tensor each
-class SdBlockState:
-    def __init__(self, h: torch.Tensor, hsp: torch.Tensor, block_info):
-        self.backbone = h
-        self.skip = hsp
-        self.block_nr = block_info[1]
-
-
-def store_block_state(unet, block_states):
-    def output_block_patch(
-        h: torch.Tensor, hsp: torch.Tensor, transformer_options
-    ):
-        state = SdBlockState(h.cpu(), hsp.cpu(), transformer_options["block"])
-        if state.block_nr not in block_states:
-            block_states[state.block_nr] = []
-        block_states[state.block_nr].append(state)
-        return h, hsp
-
-    unet_patched = unet.clone()
-    unet_patched.set_model_output_block_patch(output_block_patch)
-    return unet_patched
+        print(f"Render Features for Block {int(block_nr_slider)} at step {int(step_slider)}")
+        return self.renderer.grid(block_state.hsp)
 
 
 class FeatureRenderer:
-    def __init__(self, array):
-            self.array = array
-            self.create_heatmap()
-    
-    def render(array):
+    cols: int = 32
+
+    def grid(self, features, padding=1):
+        rows = int(len(features) / self.cols) + 1
+        heatmaps = [
+            self.feature(feature.detach().numpy())
+            for feature in features
+        ]
+        width, height = heatmaps[0].size
+        width += padding
+        height += padding
+
+        grid_canvas = Image.new("RGB", (self.cols * width, rows * height))
+
+        for i in range(rows):
+            for j in range(self.cols):
+                pos = (j * width, i * height)
+                grid_canvas.paste(heatmaps[i * self.cols + j], pos)
+
+        return [grid_canvas] + [heatmaps]
+
+    def feature(self, array):
         # Normalize the array to 0-255 range for image.
-        normalized_array = (array - np.min(array)) / (np.max(array) - np.min(array))
+        normalized_array = (array - np.min(array)) / (
+            np.max(array) - np.min(array)
+        )
         heatmap_image = (normalized_array * 255).astype(np.uint8)
-    
-        # Use a color map, e.g., jet.
-        from matplotlib import cm
-        jet_map = cm.get_cmap('jet')
-        heatmap_color = (jet_map(heatmap_image.reshape(-1))[:, :3] * 255).astype(np.uint8)
-        
+
+        jet_map = cm.get_cmap("jet")
+        heatmap_color = (
+            jet_map(heatmap_image.reshape(-1))[:, :3] * 255
+        ).astype(np.uint8)
+
         # Create PIL image from the array.
         return Image.fromarray(heatmap_color.reshape(*heatmap_image.shape, -1))
-        
-
-        
-# class HeatmapGrid:
-#     def __init__(self, size: int, caption: str = "", cols: int = 8):
-#         self.cols = cols
-#         self.size = size
-#         self.rows = int(size / cols) + int(size % cols != 0)
-#         self.caption = caption
-
-#     def setup_grid(self) -> Tuple[plt.Figure, plt.Axes]:
-#         print(f"Setting up {self.cols} x {self.rows} for {self.caption}")
-#         fig, axes = plt.subplots(
-#             nrows=self.cols,
-#             ncols=self.rows,
-#             figsize=(self.rows * 3, self.cols * 3),
-#         )
-
-#         for ax in axes.flat:
-#             ax.axis("off")
-
-#         print("Done")
-
-#         return fig, axes
-
-#     def render(self) -> Image:
-#         print(f"Rendering {self.caption}")
-#         buffer = io.BytesIO()
-#         self.fig.savefig(buffer, format="png")
-#         buffer.seek(0)
-#         image = Image.open(buffer)
-
-#         plt.close(self.fig)
-#         print("Done")
-#         return image
-    
